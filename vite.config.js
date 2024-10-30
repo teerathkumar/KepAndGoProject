0df7fe9d780acc95365d33e6ec1c3ed4e045c573
SkySystemsInc <rajas.bit@gmail.com>
2024-10-30 16:04:06 +0500

    push

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/js/app.jsx',
-                'resources/assets/css/nucleo-svg.css',
-                'resources/assets/css/soft-ui-dashboard.css?v=1.0.3'
-            ],
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
         react(),
